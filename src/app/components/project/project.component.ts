acd8ae5b8fe6b76ef7418758078804f1d501e037
Sijun Kim <junniekim12@gmail.com>
2023-12-20 23:07:53 -0500

    update

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -12,8 +12,7 @@ export class ProjectComponent {
       id: 1,
       title: "Sijun Kim's E-Portfolio",
       picture: '/assets/img/websiteShowCase.PNG',
-      tools:
-        'HTML/CSS, Typescript, Angular, Bootstrap, Visual Studio Code, GitHub',
+      tools: 'Angular, Bootstrap, VS Code, Git',
 
       duration: '13 Hours',
       description:
@@ -22,8 +21,7 @@ export class ProjectComponent {
     {
       id: 2,
       duration: '16 Hours',
-      tools:
-        'Public API, HTML/CSS, Typescript, Angular, Bootstrap, Visual Studio Code, GitHub',
+      tools: 'Public API, Angular, Bootstrap, VS Code, Git',
       title: 'PokeAPI Project',
       picture: '/assets/img/pokemonShowCase.PNG',
       description:
@@ -31,47 +29,28 @@ export class ProjectComponent {
     },
     {
       id: 3,
-      duration: '17 Hours',
-      tools:
-        'HTML/CSS, Typescript, Angular, Bootstrap, Visual Studio Code, GitHub',
-      title: 'Pizza Ordering Project',
-      picture: '/assets/img/pizzaShowCase.PNG',
+      duration: '8 Hours',
+      tools: 'Vite, VS code, Git, ,Tailwind, React',
+      title: 'Sort Visualizer',
+      picture: '/assets/img/sort.png',
       description:
-        'The purpose of this project is to get more familiar with reactive forms and template forms in Angular. I learned and understood the difference between the two, and I applied what I learned into practice. I also had a chance to write directives to check the required formats in user input such as email address. ',
+        'The purpose of this project is to visualize different sorting and observe each stage of sorting with bar graph.              Link: sort-visualizer-eight.vercel.app',
     },
     {
       id: 4,
-      duration: '21 Hours',
-      tools: 'HTML/CSS, Typescript, Angular, Bootstrap, Visual Studio, GitHub',
-      title: 'Web Application at Work I ',
-      picture: '/assets/img/work1.PNG',
-      description:
-        'This was the first prod-level web application that I made at work. Unlike my personal projects, this project had to be at the professional level, so I conducted lots of testing on it. I also had a chance to collaborate with backend developers: I got necessary end points from them and designed the web page with the provided endpoints. I also had a chance to use some angular components that my coworkers wrote. This was such a valuable experience as I learned how to better communicate and collaborate with team members to meet the goal.',
-    },
-    {
-      id: 5,
       duration: '5 Hours',
-      tools: 'Java, IntelliJ',
+      tools: 'Java',
       title: 'Genetic Algorithm on Traveling Salesperson Problem',
       picture: '/assets/img/genetic.png',
       description:
         'The purpose of this project is to learn how genetic algorithm works. I had a chance to implement reproduction of each generation through order crossover and mutation. The image above shows the result based on 100,000th generation.',
     },
+
     {
-      id: 6,
-      duration: '6 Hours',
-      tools:
-        'HTML/CSS, Typescript, Angular, Bootstrap, ngx-bootstrap, Visual Studio Code, GitHub',
-      title: 'Notes Application',
-      picture: '/assets/img/notes.png',
-      description:
-        'The purpose of this project was to solidify my basic angular skills. The app allows  users to create task/reminder notes. If it is a task, user may mark it either complete/incomplete. Completed tasks are crossed out.',
-    },
-    {
-      id: 7,
+      id: 5,
       duration: '4 Hours',
-      tools: 'HTML/CSS, Javascript, Bootstrap, Visual Studio Code, GitHub',
-      title: 'Javascript Gamepack',
+      tools: 'Vanilla Javascript, Bootstrap, VS Code, Git',
+      title: 'Vanilla Javascript Gamepack',
       picture: '/assets/img/snapshot.PNG',
       description:
         'This project allows users to play a classic hangman game, memory game, and tic-tac-toe. Each game has different features such as easy/hard mode, small hints, cheating mode.',
